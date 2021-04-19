1855c2c00b85d91bb0a2142f368c305c4bc71b4d
Tapan <tbanerjee@mdsol.com>
2021-04-19 15:55:32 -0400

    try-catch block

--- dist/index.js
+++ dist/index.js
@@ -6046,22 +6046,26 @@ const core= __nccwpck_require__(5);
 const github= __nccwpck_require__(97);
 
 async function run(){
-    console.log('Initializing for function execution...');
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');    
-    const octokit = github.getOctokit(GITHUB_TOKEN);
-    console.log('Getting context..');
-    const {context = {} } = github;
-    const { pull_request } =  context.payload;
+    try{
+        console.log('Initializing for function execution...');
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');    
+        const octokit = github.getOctokit(GITHUB_TOKEN);
+        console.log('Getting context..');
+        const {context = {} } = github;
+        const { pull_request } =  context.payload;
 
-    console.log('Starting function execution...');
+        console.log('Starting function execution...');
 
-    await octokit.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.issue_number,
-        body: 'Thank you for submitting a pull request. This will be reviewed using GitHub Actions'
-    })
-    // console.log()
-    console.log('Ending function execution...');
+        await octokit.issues.createComment({
+            ...context.repo,
+            issue_number: pull_request.issue_number,
+            body: 'Thank you for submitting a pull request. This will be reviewed using GitHub Actions'
+        })
+        // console.log()
+    }catch(error){console.log(`Error in execution: ${error.message}`)}
+    finally{
+        console.log('Ending function execution...');
+    }
 }
 
 run();
