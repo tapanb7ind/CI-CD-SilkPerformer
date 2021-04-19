6ff38da1e9cd1912d56ad75d86c05e7cce9eeea0
Tapan <tbanerjee@mdsol.com>
2021-04-19 15:31:18 -0400

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+var __webpack_exports__ = {};
+async function run(){
+    console.log('Starting function execution...');
+    // console.log()
+    console.log('Ending function execution...');
+}
+
+run();
+module.exports = __webpack_exports__;
+/******/ })()
+;
