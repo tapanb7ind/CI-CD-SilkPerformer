6e9f3b4056721a4aada60030b9732628256603e6
Tapan <tbanerjee@mdsol.com>
2021-04-19 23:59:09 -0400

    Update action.js

--- src/action.js
+++ src/action.js
@@ -56,8 +56,7 @@ async function main(){
                     }
                     else{
                         canContinue = false;
-                        core.setFailed(`PR Title validation failed. [Title:${prdata.title}, Regex: ${context.payload.inputs.prTitleTemplate}]`); 
-                        console.log(JSON.stringify(prdata));
+                        core.setFailed(`PR Title validation failed. [Title:'${prdata.title}', Regex: ${context.payload.inputs.prTitleTemplate}]`);                         
                         return;
                     }
                 }
