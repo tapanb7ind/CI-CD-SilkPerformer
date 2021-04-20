29769e8e30163340106ee1b1794adc801237a59d
Tapan <tbanerjee@mdsol.com>
2021-04-19 21:25:55 -0400

    Try workflowDispatch

--- src/action.js
+++ src/action.js
@@ -7,8 +7,8 @@ async function run(){
         const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');    
         const octokit = github.getOctokit(GITHUB_TOKEN);
         console.log('Getting context..');
-        const {context = {} } = github;
-        const { pull_request } =  context.payload;
+        const { context = {} } = github;
+        const { pull_request } =  context.payload;        
 
         console.log('Starting function execution...');
         console.log(JSON.stringify(context));
