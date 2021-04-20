6e1c7efa6b2ea999371d001721feb8b8064266e9
Tapan <tbanerjee@mdsol.com>
2021-04-19 21:49:31 -0400

    updated action to run with param

--- src/action.js
+++ src/action.js
@@ -1,6 +1,47 @@
 const core= require('@actions/core');
 const github= require('@actions/github');
 
+async function main(){
+    console.log('Initializing for function execution...');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');    
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    console.log('Getting execution context..');
+    const { context = {} } = github;
+    const { pull_request } =  context.payload;
+    let PR_NUM = 0;
+
+    switch(github?.eventName.toLowerCase()){
+        case "workflow_dispatch":
+            console.log(`[DEBUG] Executing as "workflow_dispatch". [Pull Request# ${github.payload.inputs.prNum}]`);            
+            PR_NUM = parseInt(${github.payload.inputs.prNum});            
+            break;
+        case "pull_request":
+            console.log(`[DEBUG] Executing as "pull_request". [Pull Request# ${github.payload.inputs.prNum}]`);
+            PR_NUM = parseInt(${github.payload.inputs.prNum})            
+            break;
+        case default:
+            core.setFailed(`"eventName" [${github?.eventName}] is not valid`);
+            break;
+    }
+
+    if(isNaN(PR_NUM) || PR_NUM === 0){
+        core.setFailed(`INVALID Pull-Request provided.[PR#${PR_NUM}]`);
+    }
+    else{
+        console.log(`[DEBUG] Pull-Request [${PR_NUM}] number is valid. Continuing with validation`);
+        // await GetCommitsInPR(PR_NUM);
+    }
+}
+
+async function GetPRByNumber(pull_request_number){
+
+    return null;
+}
+
+async function GetCommitsInPR(pull_request_number){
+    return null;
+}
+
 async function run(){
     try{
         console.log('Initializing for function execution...');
@@ -8,7 +49,7 @@ async function run(){
         const octokit = github.getOctokit(GITHUB_TOKEN);
         console.log('Getting context..');
         const { context = {} } = github;
-        const { pull_request } =  context.payload;        
+        const { pull_request } =  context.payload;
 
         console.log('Starting function execution...');
         console.log(JSON.stringify(context));
@@ -25,4 +66,4 @@ async function run(){
     }
 }
 
-run();
+main();
