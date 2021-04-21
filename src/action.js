82009621e4ca0ce2685216000505c570272a309e
Tapan <tbanerjee@mdsol.com>
2021-04-21 17:17:47 -0400

    comment as table - try 2

--- src/action.js
+++ src/action.js
@@ -158,8 +158,7 @@ async function main(){
 
 async function PostCommentToPR(_octokit, _owner, _repo, _pr, reason, reviewStatus){
     // let _body = `<b>Initial Review ${reviewStatus}.<b><br/>Reason<br/><li>${reason}</li>`
-    let _body=`| Status | Reason |
-    | --- | --- |` `${reviewStatus} | ${reason} |`
+    let _body=`| Status | Reason |\n| --- | --- \n|\`${reviewStatus}\`| ${reason} |`
     console.log(`[Debug] Posting comment for PR#${_pr} @ /repos/${_owner}/${_repo}/pulls/${_pr}/comments`);
     try{        
         // await _octokit.request(`POST /repos/${_owner}/${_repo}/pulls/${_pr}/comments`, {
