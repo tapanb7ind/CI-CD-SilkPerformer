3fc1511bedcb989a0c804be67b011bb393a9b4bb
Tapan <tbanerjee@mdsol.com>
2021-04-21 17:05:09 -0400

    updated comment style

--- src/action.js
+++ src/action.js
@@ -157,7 +157,7 @@ async function main(){
 }
 
 async function PostCommentToPR(_octokit, _owner, _repo, _pr, reason, reviewStatus){
-    let _body = `<b>Initial Review ${reviewStatus}.<b><br/>Reason: ${reason}`
+    let _body = `<b>Initial Review ${reviewStatus}.<b><br/>Reason<br/><li>${reason}</li>`
     console.log(`[Debug] Posting comment for PR#${_pr} @ /repos/${_owner}/${_repo}/pulls/${_pr}/comments`);
     try{        
         // await _octokit.request(`POST /repos/${_owner}/${_repo}/pulls/${_pr}/comments`, {
